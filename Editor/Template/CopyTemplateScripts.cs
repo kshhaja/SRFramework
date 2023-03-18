001bf50c437e66e22b87d8eef426817bb68a7b40
kshhaja <kshhaja@gmail.com>
2023-03-19 04:43:55 +0900

    [Refactor] if TemplateScripts folder is not exists, create directory

--- Editor/Template/CopyTemplateScripts.cs
+++ Editor/Template/CopyTemplateScripts.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEditor;
+using System.IO;
+
 
 [InitializeOnLoad]
 public class CopyTemplateScripts
@@ -11,22 +13,25 @@ public class CopyTemplateScripts
 
     static CopyTemplateScripts()
     {
-        System.IO.DirectoryInfo source = new System.IO.DirectoryInfo(GetCurrentFileName());
-        source = new System.IO.DirectoryInfo(source.Parent.FullName + "\\Scripts");
-        System.IO.DirectoryInfo dest = new System.IO.DirectoryInfo(Application.dataPath + "\\ScriptTemplates");
-        
+        DirectoryInfo source = new DirectoryInfo(GetCurrentFileName());
+        source = new DirectoryInfo(source.Parent.FullName + "\\Scripts");
+        DirectoryInfo dest = Directory.CreateDirectory(Application.dataPath + "\\ScriptTemplates");
+
         var files = source.GetFiles();
         foreach (var file in files)
         {
-            try
+            if (Path.GetExtension(file.Name) == ".meta")
             {
-                FileUtil.CopyFileOrDirectory(file.FullName, dest.FullName + file.Name); 
-            }
-            catch (System.Exception e)
-            {
-                Debug.LogError(e.Message);
                 continue;
             }
+
+            string destFullName = dest.FullName + "/" + file.Name;
+            if (File.Exists(destFullName))
+            {
+                continue; 
+            }
+
+            FileUtil.CopyFileOrDirectoryFollowSymlinks(file.FullName, destFullName);
         }
     }
 }
